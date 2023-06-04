bec645b6672fd12920300c944e6ab8ca29e1dbd3
MashaZherebtsova <113789649+MashaZherebtsova@users.noreply.github.com>
2023-06-04 16:16:50 +0300

    upd

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -3,13 +3,16 @@ import { ShopingCart } from 'components/ShopingCart';
 import { useSelector } from 'react-redux';
 import { selectTotalPrice } from 'redux/selectors';
 import { ReactComponent as EmptyCart } from 'img/emptyCart.svg';
+import { CartContainer } from './Cart.styled';
 export function Cart() {
   const price = useSelector(selectTotalPrice);
   return (
-    <div>
-      <ShopingCart />
+    <>
+      <CartContainer>
+        <ShopingCart />
+      </CartContainer>
       <p>Total Prise:{price}</p>
       {price > 0 ? <CartList /> : <EmptyCart />}
-    </div>
+    </>
   );
 }
---
+++ src/Pages/Cart/Cart.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const CartContainer = styled.div`
+  background-color: #fff;
+  border-radius: 20px;
+  margin-right: 20px;
+  padding: 20px;
+`;
